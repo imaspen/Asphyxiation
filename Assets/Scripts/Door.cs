c4de730658cc5231813beefb484fefeaa0afa4c2
G.Etherington U1874339 <U1874339@unimail.hud.ac.uk>
2018-10-25 13:42:39 +0100

    I chime in with haven't you people ever heard of, coding a goddamn door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    private bool inDoor = false;
+    [SerializeField] private string loadRoom;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Hello");
+        if (other.CompareTag("Player"))
+        {
+            inDoor = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Goodbye");
+        if (other.CompareTag("Player"))
+        {
+            inDoor = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Debug.Log(inDoor);
+            if (inDoor)
+            {
+                Debug.Log("Hey");
+                SceneManager.LoadScene(loadRoom);
+            }
+        }
+    }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
