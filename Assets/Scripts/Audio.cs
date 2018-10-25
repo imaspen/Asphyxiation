947ecee8eac8637e531d0f8d5e20cc92590131ca
Aspen <zebington@gmail.com>
2018-10-25 14:00:28 +0100

    added sound files

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour {
+    private int bpm = 120;
+
+
+	// Use this for initialization
+	void Start () {
+        var sources = gameObject.GetComponentsInChildren(typeof(AudioSource));
+        double startTime = AudioSettings.dspTime + 2;
+        foreach (AudioSource source in sources)
+        {
+            source.PlayScheduled(startTime);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
